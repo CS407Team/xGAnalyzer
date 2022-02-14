b7a39f6a296386817ce3eb007f713c435eaa2c2d
elielvipata <vkilembo@gmail.com>
2022-02-14 19:50:36 +0000

    inserting tournaments into tables

--- data_files/insert_league.py
+++ data_files/insert_league.py
@@ -4,16 +4,13 @@ from mysql.connector import errorcode
 try:
     db_connection = mariadb.connect(user = 'newuser', password='test', host='localhost', database = 'analyzer', port='3306')
     db_cursor = db_connection.cursor()
-    db_cursor.execute("insert into tournaments(name,total_teams,country, num_previous_seasons) values('Premier League', '20', 'England','5')")
+    sql = """insert into tournaments (name,total_teams, country, num_previous_seasons)values('Premier League', 20, 'England',5)"""
+    db_cursor.execute(sql)
 
 except mariadb.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
+	print("Something went wrong: {}".format(err))
 else:
+    db_connection.commit()
     print("Done");
     db_connection.close()
     

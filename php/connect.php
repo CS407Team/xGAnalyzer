67836bb8fba51ab2a35d0847af157d23ad0e88a9
elielvipata <vkilembo@gmail.com>
2022-02-19 16:36:57 +0000

    added connect file for php

---
+++ php/connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "V3z?RnzC";
+// Create connection
+$conn = new mysqli($servername, $username, $password,"analyzer");
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+ echo "Connected successfully";
+?>
+

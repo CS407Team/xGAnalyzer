77e69a57742408c54507119a1ea7cbd4b0f6c051
eliel <vkilembo@gmail.com>
2022-03-05 10:55:12 -0500

    created data files for all fixtures and season tables upto 2015

---
+++ collect_all_data/fixtures/get_fixtures.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+url = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
+
+leagues = ["premier_league", "bundesliga","laliga","ligue1","serie_a"]
+
+seasons = ["2021","2020","2019","2018", "2017","2016","2015"]
+
+headers = {
+    'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+    'x-rapidapi-key': "f6a74521femsh3da9a6c760ad94dp1ff3e9jsn7e739e41cf69"
+    }
+
+for season in seasons:
+    querystring = {"league":"61","season":season}
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response_dictionary = json.loads(response.text)
+    file_name = "ligue1/"+season
+    file = open(file_name,"a")
+    file.write(json.dumps(response_dictionary["response"]))
+    file.close()   

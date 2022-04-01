45ee8782844b84cc8fd861c109badbb5e689c44f
nguynguy1 <75349287+nguynguy1@users.noreply.github.com>
2022-04-01 17:58:02 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 from io import BytesIO
 from app import modules as modules
 from app import predict as predict
+from app import teams as teams
 from app import config
 import app.apple as apple
 from flask import Blueprint, render_template, request, redirect, url_for
@@ -29,15 +30,16 @@ import uuid
 import base64
 from os import walk
 
+UPLOAD_FOLDER = '..\images'
 app = Flask(__name__, template_folder='html')
 
 app.debug = True
 leagues = modules.get_league()
 imageExtensions = ['png', 'jpg']
 
-
 @app.route("/", methods=["POST", "GET"])
 def home():
+    pos = []
     if request.method == "POST":
         username = request.form.get('username')
         print(username)
@@ -48,25 +50,76 @@ def home():
             user = modules.get_user(str(username), str(password))[0]
         if user is None:
             return render_template('notfound.html', message="Username or Password not found")
-        return redirect(url_for('userpage', userid=int(user[0]), username=str(user[1]), leagueid=1))
+        return redirect(url_for('userpage', userid=int(user[0]), username=str(user[1]), leagueid=1, season='2021'))
     else:
         return render_template('login.html')
 
 
-@app.route("/userpage/<userid>/<username>/<leagueid>", methods=["POST", "GET"])
-def userpage(userid, username, leagueid):
-    teams = modules.get_team(leagueid)
+@app.route("/userpage/<userid>/<username>/<leagueid>/<season>", methods=["POST", "GET"])
+def userpage(userid, username, leagueid, season):
+    tems = modules.get_team(leagueid)
+    pos = []
+    pos = pos.sort()
     if request.method == "POST":
         tourid = request.form.get('league')
+        fav_team = request.form.get('team')
+        season = str(request.form.get('season'))
+        if season == '2020' and not teams.teams_2020.__contains__(teams.teams_2021[int(fav_team)]):
+            return render_template('notfound.html', message="team were not there that season")
+        modules.insert_fav_team(userid,fav_team)
         for league in leagues:
             if int(league['leagueid']) == int(tourid):
                 leagues.remove(league)
                 leagues.insert(0, league)
         return redirect(url_for('userpage', userid=int(userid),
-                                username=str(username), leagueid=int(tourid)))
+                                username=str(username), leagueid=int(tourid), season=season,pos=pos))
     else:
+        fav_teams = modules.get_fav_team(userid)
+        favs = []
+        for f in fav_teams:
+            favs.append(teams.teams_2021[f])
+        print(str(fav_teams))
+        fav_team_res = []
+        fav_team_pos = []
+        print("season " + str(season))
+        print("length" + str(len(fav_teams)))
+        for team in fav_teams:
+            fav_res = modules.get_stat(team, season)
+            fav_pos = modules.get_fav_pos(team, season)
+            print("favpos " + str(fav_pos))
+            print(str(fav_res))
+            for fav in fav_res:
+                home_in = int(fav[0]) % 100 + int(fav[0]) // 10
+                away_in = int(fav[1]) % 100 + int(fav[1]) // 10
+                home = None
+                away = None
+                if str(season) == '2021':
+                    home = teams.teams_2021[home_in]
+                    away = teams.teams_2021[away_in]
+                elif str(season) == '2020':
+                    home = teams.teams_2020[home_in]
+                    away = teams.teams_2020[away_in]
+                elif str(season) == '2019':
+                    home = teams.teams_2019[home_in]
+                    away = teams.teams_2019[away_in]
+                elif str(season) == '2018':
+                    home = teams.teams_2018[home_in]
+                    away = teams.teams_2018[away_in]
+                elif str(season) == '2017':
+                    home = teams.teams_2017[home_in]
+                    away = teams.teams_2017[away_in]
+                if int(fav[2]) != -1 and int(fav[3]) != -1:
+                    fav_team_res.append([home, away, fav[2], fav[3], fav[4]])
+            fav_team_pos.append(fav_pos[0])
         return render_template('userpage.html', userid=int(userid), username=str(username), leagueid=int(leagueid),
-                               leagues=leagues, teams=teams)
+                               season=season, pos=pos,
+                               leagues=leagues, teams=tems, fav_team_res=fav_team_res, fav_team_pos=fav_team_pos, favs=favs)
+
+@app.route("/table/<userid>/<username>/<leagueid>/<pos>")
+def table(userid, username, leagueid, pos):
+    return render_template('table.html',userid=userid,username=username,leagueid=leagueid,pos=pos)
+
+
 
 
 @app.route('/uploadpage/<userid>/<username>/<leagueid>')
@@ -103,6 +156,7 @@ def upload_image(userid, username, leagueid):
             except IOError:
                 return render_template('notfound.html', message="xG graph is not a valid image file")
             if str(hometeam) != "" and str(awayteam) != "" and str(hometeam) != "None" and str(awayteam) != "None":
+                # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 filename = "https://storage.googleapis.com/xganalyzer_exports/games/" + filename
                 homeid = hometeam.split(',')[0]
                 homename = hometeam.split(',')[1]
@@ -118,8 +172,8 @@ def upload_image(userid, username, leagueid):
                 awaylastten = getLastTen(awayid)
                 awayresten = getResTen(awaylastten)
                 prediction = predict.predctXG(homeid, awayid, homename, awayname, homelastten,
-                                              awaylastten, homeresten, awayresten, filename, maxXG)
-                return render_template('prediction.html', filename=filename,
+                                              awaylastten, homeresten, awayresten, file, maxXG)
+                return render_template('prediction.html',
                                        userid=int(userid), username=str(username), leagueid=int(leagueid),
                                        teams=teams, homename=homename, awayname=awayname, prediction=prediction[0],
                                        analysis=prediction[1])
